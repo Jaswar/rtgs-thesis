0c7e741ddce7008f3f0896b6a4bd58c4b5945bce
Jan Warchocki <janwarchocki@gmail.com>
2025-05-02 22:22:01 +0200

    Remove seed setting

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -162,9 +162,9 @@ def safe_state(silent):
 
     sys.stdout = F(silent)
 
-    random.seed(0)
-    np.random.seed(0)
-    torch.manual_seed(0)
+    # random.seed(0)
+    # np.random.seed(0)
+    # torch.manual_seed(0)
     torch.cuda.set_device(torch.device("cuda:0"))
     
 def knn(x, src, k, transpose=False):
